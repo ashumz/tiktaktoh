c0460899700112b12628bfe88bdd4688b689371e
Ashley McClelland <ashley@disneyland.(none)>
2010-04-22 23:14:02 -0400

    all possible wins added via checkWin(), cat's game added

--- public/js/ttt.js
+++ public/js/ttt.js
@@ -1,14 +1,20 @@
-function init() { 
-  var X = "x", 
-      O = "o",
-      EMPTY="",
-      currentMark=X;
+var X = "x", 
+		O = "o",
+		EMPTY="",
+		currentMark;
 
-  var grid=[[EMPTY, EMPTY, EMPTY],
-            [EMPTY, EMPTY, EMPTY],
-            [EMPTY, EMPTY, EMPTY]];
-  
-  var table=document.createElement("table");
+var grid;
+
+function init() {
+	currentMark = X;
+	grid=[[EMPTY, EMPTY, EMPTY],
+				[EMPTY, EMPTY, EMPTY],
+				[EMPTY, EMPTY, EMPTY]];
+
+  var contentDiv = document.getElementById("content");
+ 	contentDiv.innerHTML="";
+	
+	var table=document.createElement("table");
   
   
   for(var i=0; i < grid.length; i++) {
@@ -17,47 +23,102 @@ function init() {
     table.appendChild(tr) 
 
   
-   for(var j=0; j < grid[i].length; j++) {
+ 		for(var j=0; j < grid[i].length; j++) {
       
       var td=document.createElement("td");
       td.id = "cell-" + i + "-" + j; 
       td.appendChild(document.createTextNode(grid[i][j]));
       tr.appendChild(td);
-
-      td.addEventListener("click", function () { 
-        var parts=this.id.split(/-/), 
-            i=parseInt(parts[1],10),	
-	    j=parseInt(parts[2],10);
-
-//console.log(this.id, parts, i, j)
-        
-	if (grid[i][j]!=EMPTY)
-            return;     
-
-	//populating grid
-        grid[i][j]=currentMark;
-
-        this.appendChild(document.createTextNode(currentMark));
-        if (currentMark==X) {
-          currentMark=O;
-        } else if (currentMark=O) {
-          currentMark=X;       
-        }
-       
-        
-
-      /* result = (condition ? consequent : alternative)
-       currentMark= (currentMark==X ? O : X) */
-
-      }, false);
+      td.addEventListener("click", gameMove, false);
     }
   }
 
- 
-  var contentDiv = document.getElementById("content");
+
+
   contentDiv.appendChild(table);
 
 
 }
 
+ function gameMove () { 
+        var parts=this.id.split(/-/), 
+            i=parseInt(parts[1],10),	
+	          j=parseInt(parts[2],10);
 
+//console.log(this.id, parts, i, j)
+        
+	  	  if (grid[i][j]!=EMPTY)
+            return;     
+
+	//populating grid
+        grid[i][j] = currentMark;
+
+        this.appendChild(document.createTextNode(currentMark));
+				if (currentMark==X) {
+          currentMark=O;
+        } else {
+					currentMark=X;       
+        }
+        checkWin();
+				noWinner();
+
+      /* result = (condition ? consequent : alternative)
+       currentMark= (currentMark==X ? O : X) */
+
+}
+
+function checkWin () {
+	
+	if (grid[0][0] == grid [0][1] && grid[0][1] == grid[0][2] && grid[0][0] !=EMPTY){
+			winner(grid [0][1]); 
+	}
+
+	if (grid[1][0] == grid [1][1] && grid[1][1] == grid[1][2] && grid[1][0] !=EMPTY){
+			winner(grid [1][1]); 
+ 	}
+
+	if (grid[2][0] == grid [2][1] && grid[2][1] == grid[2][2] && grid[2][0] !=EMPTY){
+			winner(grid [2][1]); 
+ 	}
+
+	if (grid[0][0] == grid [1][0] && grid[1][0] == grid[2][0] && grid[0][0] !=EMPTY){
+			winner(grid [0][0]); 
+	}
+	if (grid[0][1] == grid [1][1] && grid[1][1] == grid[2][1] && grid[0][1] !=EMPTY){
+			winner(grid [0][1]); 
+	}
+	if (grid[0][2] == grid [1][2] && grid[1][2] == grid[2][2] && grid[0][2] !=EMPTY){
+			winner(grid [0][2]); 
+	}
+	if (grid[0][0] == grid [1][1] && grid[1][1] == grid[2][2] && grid[2][2] !=EMPTY){
+			winner(grid [2][2]); 
+	}
+
+	if (grid[0][2] == grid [1][1] && grid[1][1] == grid[2][0] && grid[2][0] !=EMPTY){
+			winner(grid [2][0]); 
+	}
+}
+
+function winner (whoWon) {
+	alert(whoWon + " won!"); 
+	resetBoard();
+}
+
+//cat's game-- checkWin() conditions not met after 9 clicks(if gameMove().call.9times)
+function noWinner () { 
+	for(var i=0; i < grid.length; i++){
+		for	(var j=0; j < grid[i].length; j++){ 
+			if (grid[i][j]==EMPTY){
+				return;
+			}	
+		}
+	}	
+	alert("The only way to win is to not play the game.")
+	resetBoard();
+}		
+ 
+
+function resetBoard () {
+	init();
+}
+	
