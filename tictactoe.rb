236f015cf03cf3e2c6f6a29856b5e78e6a78438d
Ashley McClelland <ashley@disneyland.(none)>
2010-05-06 18:08:52 -0700

    completed tiktaktoh, structure set for adding new games

--- tictactoe.rb
+++ tictactoe.rb
@@ -2,7 +2,21 @@ require "rubygems"
 require "sinatra"
 
 get '/' do
-erb :layout
+	erb :tictactoe
 end
 
+get '/tictactoe' do
+	erb :tictactoe
+end
 
+get '/hangman' do
+	erb :hangman
+end
+
+get '/bingo' do
+	erb :bingo
+end
+
+get '/scramble' do
+	erb  :scramble
+end
